9817e64af590fd8bc3346d5e9e65b854f12f5515
Nidhish Krishnan <nidhishkrishnan@gmail.com>
2018-08-17 10:56:17 +0530

    Update SamplePro1Application.java

--- sample-pro1/src/main/java/com/jenkins/samplepro1/SamplePro1Application.java
+++ sample-pro1/src/main/java/com/jenkins/samplepro1/SamplePro1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SamplePro1Application {
 	public static void main(String[] args1) {
+		
 		SpringApplication.run(SamplePro1Application.class, args1);
 	}
 }
